70e341295eadfb9bcbb230f9e6c410972f1bd54e
sunny00000000 <96020222+sunny00000000@users.noreply.github.com>
2022-03-08 14:10:42 +0530

    git commit

--- git.sh
+++ git.sh
@@ -1,3 +1,5 @@
 echo "installing git :"
 echo "git.sh :"
 echo "coping the file :"  cp maven-web-application maven-standalone-application
+echo "contoll the process of the declerative and prodective access :"
+
